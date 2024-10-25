6f24cb7dd579b5f0ae88f5f1e549e604375ff84e
eunju98 <dmswn1321@gmail.com>
2024-10-25 13:07:35 +0900

    자리에서 수정작업1차

--- resources/js/javascript.js
+++ resources/js/javascript.js
@@ -1,10 +1,10 @@
 // Swiper initialization
 const swiper = new Swiper('.swiper-container', {
     loop: true, // Enable looping
-    autoplay: {
-        delay: 3000, // Slide changes every 3 seconds
-        disableOnInteraction: false, // Keep autoplay even after user interaction
-    },
+    // autoplay: {
+    //     delay: 3000, // Slide changes every 3 seconds
+    //     disableOnInteraction: false, // Keep autoplay even after user interaction
+    // },
     pagination: {
         el: '.swiper-pagination',
         clickable: true, // Pagination dots are clickable
