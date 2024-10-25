b33a076677ef7cff3e680a4935a60d6aba41f5ea
eunju98 <dmswn1321@gmail.com>
2024-10-25 09:45:52 +0900

    first commit

---
+++ resources/js/javascript.js
@@ -0,0 +1,16 @@
+// Swiper initialization
+const swiper = new Swiper('.swiper-container', {
+    loop: true, // Enable looping
+    autoplay: {
+        delay: 3000, // Slide changes every 3 seconds
+        disableOnInteraction: false, // Keep autoplay even after user interaction
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true, // Pagination dots are clickable
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
